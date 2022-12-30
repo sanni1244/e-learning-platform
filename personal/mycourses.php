653b21c1734b9274642ddf4755fbc65f9ecad978
sanni1244 <opeyemi.sanni180591052@st.lasu.edu.ng>
2022-12-30 15:45:31 +0100

    style in a new file

--- personal/mycourses.php
+++ personal/mycourses.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/vent/style/style2.css">
-<!-- <link rel="stylesheet" href="/vent/style/bootstrap.css"> -->
+<link rel="stylesheet" href="/vent/style/style3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <body class="b4">
 <?php 
