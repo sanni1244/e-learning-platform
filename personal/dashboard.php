c6ee3fe9730b9a930be4d1155480126896a812be
sanni1244 <opeyemi.sanni180591052@st.lasu.edu.ng>
2023-01-14 06:11:17 +0100

    classy

--- personal/dashboard.php
+++ personal/dashboard.php
@@ -1,7 +1,11 @@
 
 <link rel="stylesheet" href="/vent/style/style2.css">
+<link rel="stylesheet" href="/vent/style/dashboard.css">
+
 <!-- <link rel="stylesheet" href="/vent/style/bootstrap.css"> -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
 
 <body>
 <?php
@@ -42,281 +46,34 @@ include_once("../personal/userdata.php");
             <div class="tip2">Welcome to yeet knowledge, it's time to learn.</div>
             
             <?php
-                $fac = $array1["faculty"];
-                $dep = $array1["department"];
-                $lev = $array1["level"];
-
-              if ($fac == '01'){
-                $fac = "FACULTY OF EDUCATION";
-                if($dep == '001'){
-                  $dep = 'EDUCATIONAL MANAGEMENT (ACCOUNTING EDUCATION)';
-                }if($dep == '002'){
-                  $dep = 'ARABIC EDUCATION';
-                }if($dep == '003'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (BIOLOGY)';
-                }if($dep == '004'){
-                  $dep = 'EDUCATIONAL MANAGEMENT (BUSINESS EDUCATION)';
-                }if($dep == '005'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (CHEMISTRY)';
-                }if($dep == '006'){
-                  $dep = 'CHRISTIAN RELIGIOUS STUDIES EDUCATION';
-                }if($dep == '007'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (COMPUTER SCIENCE)';
-                }if($dep == '008'){
-                  $dep = 'ECONOMICS EDUCATION';
-                }if($dep == '009'){
-                  $dep = 'ENGLISH EDUCATION';
-                }if($dep == '010'){
-                  $dep = 'PHYSICAL & HEALTH EDUCATION';
-                }if($dep == '011'){
-                  $dep = 'ISLAMIC RELIGIOUS STUDIES EDUCATION';
-                }if($dep == '012'){
-                  $dep = 'EDUCATIONAL FOUNDATION';
-                }if($dep == '013'){
-                  $dep = 'EDUCATIONAL MANAGEMENT';
-                }if($dep == '014'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (EDUCATIONAL TECHNOLOGY)';
-                }if($dep == '015'){
-                  $dep = 'FRENCH EDUCATION';
-                }if($dep == '016'){
-                  $dep = 'GEOGRAPHY EDUCATION';
-                }if($dep == '017'){
-                  $dep = 'HEALTH EDUCATION';
-                }if($dep == '018'){
-                  $dep = 'HISTORY EDUCATION';
-                }if($dep == '019'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (MATHEMATICS)';
-                }if($dep == '020'){
-                  $dep = 'POLITICAL SCIENCE EDUCATION';
-                }if($dep == '021'){
-                  $dep = 'SCIENCE AND TECHNOLOGY EDUCATION (PHYSICS)';
-                }if($dep == '022'){
-                  $dep = 'YORUBA EDUCATION';
-                }if($dep == '023'){
-                  $dep = 'EARLY CHILDHOOD AND PRIMARY EDUCATION';
-                }if($dep == '024'){
-                  $dep = 'CURRUCULUM STUDIES';
-                }if($dep == '025'){
-                  $dep = 'GUIDANCE AND COUNSELING EDUCATION';
-                }if($dep == '026'){
-                  $dep = 'SPECIAL EDUCATION';
-                }
-              }
-              if ($fac == '02'){
-                $fac = "FACULTY OF ENGINEERING";
-                if($dep == '001'){
-                  $dep = 'CHEMICAL ENGINEERING';
-                }if($dep == '002'){
-                  $dep = 'ELECTRONICS AND COMPUTER ENGINEERING';
-                }if($dep == '003'){
-                  $dep = 'MECHANICAL ENGINEERING';
-                }if($dep == '004'){
-                  $dep = 'CIVIL ENGINEERING';
-                }if($dep == '005'){
-                  $dep = 'AERONAUTICS AND ASTRONAUTICS ENGINEERING';
-                }}
-                if ($fac == '03'){
-                $fac = "FACULTY OF ARTS";
-                if($dep == '001'){
-                  $dep = 'ARABIC';
-                }
-                if($dep == '002'){
-                  $dep = 'CHRISTIAN RELIGIOUS STUDIES';
-                }if($dep == '003'){
-                  $dep = 'ENGLISH LANGUAGE';
-                }if($dep == '004'){
-                  $dep = 'ENGLISH LITERATURE';
-                }if($dep == '005'){
-                  $dep = 'FRENCH';
-                }if($dep == '006'){
-                  $dep = 'HISTORY AND INTERNATIONAL STUDIES';
-                }if($dep == '007'){
-                  $dep = 'ISLAMIC RELIGIOUS STUDIES';
-                }if($dep == '008'){
-                  $dep = 'MUSIC';
-                }if($dep == '009'){
-                  $dep = 'PHILOSOPHY';
-                }if($dep == '010'){
-                  $dep = 'PORTUGUESE / ENGLISH';
-                }if($dep == '011'){
-                  $dep = 'THEATRE ART';
-                }if($dep == '012'){
-                  $dep = 'LINGUISTICS';
-                }}
-              if ($fac == '04'){
-                $fac = "FACULTY OF LAW";
-                if($dep == '001'){
-                  $dep = 'ISLAMIC AND COMMON LAW';
-                }
-                if($dep == '002'){
-                  $dep = 'LAW';
-                }
-              }
-              if ($fac == '05'){
-                $fac = "FACULTY OF SCIENCE";
-                if($dep == '001'){
-                  $dep = 'BIOCHEMISTRY';
-                }if($dep == '002'){
-                  $dep = 'BOTANY';
-                }if($dep == '003'){
-                  $dep = 'CHEMISTRY';
-                }if($dep == '004'){
-                  $dep = 'COMPUTER SCIENCE';
-                }if($dep == '005'){
-                  $dep = 'FISHERIES AND AQUATIC BIOLOGY';
-                }if($dep == '006'){
-                  $dep = 'MATHEMATICS';
-                }if($dep == '007'){
-                  $dep = 'MICROBIOLOGY';
-                }if($dep == '008'){
-                  $dep = 'ZOOLOGY';
-                }if($dep == '009'){
-                  $dep = 'SCIENCE LABORATORY TECHNOLOGY';
-                }
-              }
-              if ($fac == '06'){
-                $fac = "FACULTY OF SOCIAL SCIENCE";
-                if($dep == '001'){
-                  $dep = 'ECONOMICS';
-                }
-                if($dep == '002'){
-                  $dep = 'GEOGRAPHY AND PLANNING';
-                }if($dep == '003'){
-                  $dep = 'POLITICAL SCIENCE';
-                }if($dep == '004'){
-                  $dep = 'PSYCHOLOGY';
-                }if($dep == '005'){
-                  $dep = 'SOCIOLOGY';
-                }}
-              if ($fac == '07'){
-                $fac = "FACULTY OF CLINICAL SCIENCES";
-                if($dep == '001'){
-                  $fac = 'MEDICINE AND SURGERY';
-                }if($dep == '002'){
-                  $dep = 'NURSING';
-                }}
-              if ($fac == '08'){
-                $fac = "FACULTY OF MANAGEMENT SCIENCE";
-                if($dep == '001'){
-                  $dep = 'ACCOUNTING';
-                }if($dep == '002'){
-                  $dep = 'BANKING AND FINANCE';
-                }if($dep == '003'){
-                  $dep = 'BUSINESS ADMINISTRATION';
-                }if($dep == '004'){
-                  $dep = 'INDUSTRIAL RELATIONS & HUMAN RESOURCES MANAGEMENT';
-                }if($dep == '005'){
-                  $dep = 'INSURANCE';
-                }if($dep == '006'){
-                  $dep = 'MANAGEMENT TECHNOLOGY';
-                }if($dep == '007'){
-                  $dep = 'MARKETING';
-                }if($dep == '008'){
-                  $dep = 'PUBLIC ADMINISTRATION';
-                }if($dep == '009'){
-                  $dep = 'LOCAL GOVERNMENT DEVELOPMENT AND ADMINISTRATION';
-                }if($dep == '010'){
-                  $dep = 'TAXATION STUDIES';
-                }
-              }
-              if ($fac == '09'){
-                $fac = "SCHOOL OF COMMUNICATION";
-                if($dep == '001'){
-                  $dep = 'BOOK PUBLISHING';
-                }if($dep == '002'){
-                  $dep = 'CINEMATOGRAPHY';
-                }if($dep == '003'){
-                  $dep = 'COMMUNICATION TECHNOLOGY';
-                }if($dep == '004'){
-                  $dep = 'HUMAN COMMUNICATION';
-                }if($dep == '005'){
-                  $dep = 'JOURNALISM';
-                }if($dep == '006'){
-                  $dep = 'MASS COMMUNICATION';
-                }if($dep == '007'){
-                  $dep = 'PHOTOJOUNALISM';
-                }if($dep == '009'){
-                  $dep = 'PUBLIC RELATIONS AND ADVERTISING';
-                }if($dep == '010'){
-                  $dep = 'RADIO AND TELEVISION BROADCAST';
-                }
-              }
-              if ($fac == '10'){
-                $fac = "SCHOOL OF TRANSPORT AND LOGISTICS";
-                if($dep == '001'){
-                  $dep = 'TRANSPORT MANAGEMENT AND OPERATIONS';
-                }
-              }
-              if ($fac == '11'){
-                $fac = "FACULTY OF BASIC MEDICAL SCIENCES";
-                if($dep == '001'){
-                  $dep = 'PHYSIOLOGY';
-                }}
-              if ($fac == '12'){
-                $fac = "FACULTY OF DENTISTRY";
-                if($dep == '001'){
-                  $dep = 'DENTISTRY';
-                }
-              }
-              if ($fac == '13'){
-                $fac = "SCHOOL OF AGRICULTURE";
-                if($dep == '001'){
-                  $dep = 'AGRICULTURE';
-                }if($dep == '002'){
-                  $dep = 'AGRICULTURAL ECONOMICS AND FARM MANAGEMENT';
-                }if($dep == '003'){
-                  $dep = 'AGRICULTURAL EXTENTION AND RURAL MANAGEMENT';
-                }if($dep == '004'){
-                  $dep = 'ANIMAL SCIENCE';
-                }if($dep == '005'){
-                  $dep = 'CROP PRODUCTION';
-                }
-              }
-              if ($fac == '14'){
-                $fac = "FACULTY OF BASIC CLINICAL SCIENCES";
-                if($dep == '001'){
-                  $dep = 'PHARMACOLOGY';
-                }
-              }
-              if ($fac == '15'){
-                $fac = "SCHOOL OF LIBRARY, ARCHIVAL AND INFORMATION SCIENCE";
-                if($dep == '001'){
-                  $dep = 'LIBRARY AND INFORMATION SCIENCE';
-                }
-              }
-              if ($fac == '16'){
-                $fac = "FACULTY OF PHARMACY";
-                if($dep == '001'){
-                  $dep = 'PHARMACY';
-                }
-              }
-              if ($fac == '17'){
-                $fac = "FACULTY OF ALLIED HEALTH SCIENCES";
-                if($dep == '001'){
-                  $dep = 'PHYSIOTERAPHY';
-                }
-                if($dep == '002'){
-                  $dep = 'RADIOGRAPHY';
-                }if($dep == '003'){
-                  $dep = 'MEDICAL LABORATORY SCIENCE';
-                }
-              }
-                echo ("<h2 class='fac'>" .ucfirst(strtolower($fac)) . "</h2>");
-                echo ("<h2 class='fac'>" .ucfirst(strtolower($dep)) . " (". $lev ." level) </h2>");
-
+              include('./faculties.php');
             ?>
             
         </div>
-        <div class="">
-          <h3>Last Read</h3>
-        <?php echo $array1["lastRead"];
-        
-        
-        
-        
-        
-        
-        ?>
+<?php 
+$lr = $array1["lastRead"];
+  $query = "SELECT * from `courses` where `Course Code` = '$lr'";
+$lr_run = mysqli_query($conn, $query);
+$ry = mysqli_fetch_assoc($lr_run);
+$code = $ry['Course Code'];
+$level = $ry['level'];
+$title = $ry['Course Title'];
+
+
+?>
+        <div class="dashcontent">
+          <h3 class="lst_read">Last Read</h3>
+          <h5 class="lst_read_content">
+            <img src="../images/read.png" class="read" alt="">
+            <span class="lst_read_content_text">
+              <?php echo "<span><span class='lst_read'>".$code.": ".$title."</span><br/><span class='lst_read'>".$level."</span></span>";?>
+            </span>
+            <span class="lst_read_content_text">
+asas
+            </span>
+          </h5>
+      
+    
         
       
         </div>
